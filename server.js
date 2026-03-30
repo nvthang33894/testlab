7246684891dee80093ccfec314a70e5ca935c62b
nguyenthang338 <nguyenthang33894@gmail.com>
2026-03-30 14:37:01 +0700

    add public ip o day

--- server.js
+++ server.js
@@ -1,48 +1,12 @@
 const express = require("express");
-const { exec } = require("child_process");
 
 const app = express();
-const { execFile } = require("child_process");
+const PORT = 3000;
 
-const SECRET = 'mykey';
-
-app.post("/deploy", (req, res) => {
-  const token = req.headers["x-api-key"];
-  if (token !== SECRET) {
-    return res.status(401).send("Unauthorized");
-  }
-
-  const {
-    customer,
-    app,
-    environment,
-    namespace,
-    image,
-    tag,
-    branch,
-    commit,
-    repo_url
-  } = req.body;
-
-  // validate cơ bản
-  if (!customer || !app || !namespace || !image || !tag) {
-    return res.status(400).send("Missing required params");
-  }
-
-  // sanitize (tránh command injection)
-  const safe = (v) => String(v).replace(/[^a-zA-Z0-9-_./:]/g, "");
-
-  const args = [
-    safe(customer),
-    safe(app),
-    safe(environment || "production"),
-    safe(namespace),
-    safe(image),
-    safe(tag),
-    safe(branch || "main"),
-    safe(commit || "")
-  ];
-
-  execFile("bash", ["/home/ubuntu/deploy.sh", ...args], callback)
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
-app.listen(3000, () => console.log("Deploy API running on port 3000"));
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
