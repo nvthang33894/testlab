4f70a1f63a5545e62e0ca693ef0dfde117f497fe
nvthang33894 <nvthang33894@gmail.com>
2026-04-01 13:25:44 +0700

    fix 1 version

--- a-service/server.js
+++ a-service/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World, Hello World,9aeb1acb3d8b245db442769c4a6355  ");
+  res.send("Hello World  ");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
