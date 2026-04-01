7260876073ddfb3c8ab49968325061251381d39a
nvthang33894 <nvthang33894@gmail.com>
2026-04-01 13:57:46 +0700

    update code

--- a-service/server.js
+++ a-service/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World, fix lai format, dug ghrc.io ");
+  res.send("Hello World, fix lai format, dug ghrc.iofa ");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
