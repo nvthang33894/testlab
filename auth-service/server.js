c292e2a0df7d265294fd6536dbd1996046f953e3
nvthang33894 <nvthang33894@gmail.com>
2026-04-01 18:52:24 +0700

    them folder authen services

---
+++ auth-service/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World, fix lai format, dug ghrc.iofa ");
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
