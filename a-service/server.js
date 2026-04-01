1d37e432934cb2c58b53585c35995e4d1a7852ff
nvthang33894 <nvthang33894@gmail.com>
2026-04-01 13:32:27 +0700

    oke hon

--- a-service/server.js
+++ a-service/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World  ");
+  res.send("Hello World, fix lai format ");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
