0968054bc99162e42e9df1e59bb19f45c2446347
nvthang33894 <nvthang33894@gmail.com>
2026-04-01 19:36:06 +0700

    updat eservice-b

--- service-a/server.js
+++ service-a/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World, fix lai format, dug ghrc.iofa update, fix fasdfafasfxem co chay dung khongfafaf fix  ");
+  res.send("Hello World, fix lai format, Hello world b service  ");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
