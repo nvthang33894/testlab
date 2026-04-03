e53df891eb88fe0cfb4133e77f1580f1e42b88ff
nvthang33894 <nvthang33894@gmail.com>
2026-04-03 11:08:56 +0700

    update service A o day

--- service-a/server.js
+++ service-a/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World, fix lai format, Service A   ");
+  res.send("Hello World, fix lai format, Service A, service A  ");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
